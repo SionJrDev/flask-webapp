53f5bd47990ff5a28798f309ef7b645a6493dbd3
SionJrDev <sionfernandes05@gmail.com>
2025-03-18 12:10:10 +0530

    Initial commit - Guess the Number Flask app

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+# Fixed number for testing
+secret_number = 33
+
+@app.route('/', methods=['GET', 'POST'])
+def guess_number():
+    message = ""
+    if request.method == 'POST':
+        try:
+            user_guess = int(request.form['guess'])
+            if user_guess < secret_number:
+                message = "Too low! Try again."
+            elif user_guess > secret_number:
+                message = "Too high! Try again."
+            else:
+                message = "Congratulations! You guessed the correct number."
+        except ValueError:
+            message = "Please enter a valid number."
+    
+    return render_template('index.html', message=message)
+
+if __name__ == '__main__':
+    app.run(debug=True)

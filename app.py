e0af3e09340ccdefb1181451ab2977e9eef13018
SionJrDev <sionfernandes05@gmail.com>
2025-07-17 22:40:08 +0530

    Changed to birthday page

--- app.py
+++ app.py
@@ -1,26 +1,11 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Fixed number for testing
-secret_number = 10
-
-@app.route('/', methods=['GET', 'POST'])
-def guess_number():
-    message = ""
-    if request.method == 'POST':
-        try:
-            user_guess = int(request.form['guess'])
-            if user_guess < secret_number:
-                message = "Too low! Try again."
-            elif user_guess > secret_number:
-                message = "Too high! Try again."
-            else:
-                message = "Congratulations! You guessed the correct number."
-        except ValueError:
-            message = "Please enter a valid number."
-    
-    return render_template('index.html', message=message)
+@app.route('/')
+def birthday():
+    return render_template('birthday.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
